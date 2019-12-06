847ad56852b783ebcac817a2fa031a0445de6b29
gradikay <gradi_kayamba@yahoo.com>
2019-12-06 17:08:01 -0500

    Updates

---
+++ precache-manifest.e773c050a1680e8e3b13029350b6ac52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa737519ec2e86c7cf7026ed59702514",
+    "url": "/kinid/index.html"
+  },
+  {
+    "revision": "5e5493cbf4b5259448d3",
+    "url": "/kinid/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1b5153d86a40f2344615",
+    "url": "/kinid/static/js/2.e275b438.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/kinid/static/js/2.e275b438.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5e5493cbf4b5259448d3",
+    "url": "/kinid/static/js/main.44663899.chunk.js"
+  },
+  {
+    "revision": "08e3617e2442683684f9",
+    "url": "/kinid/static/js/runtime-main.d580ea9f.js"
+  }
+]);
